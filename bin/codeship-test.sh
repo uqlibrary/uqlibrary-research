c3d5037181c190a29535ba8e85df105e03f81f73
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-04-03 13:11:14 +1000

    More test fixes

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -3,9 +3,12 @@
 echo "Testing branch: ${CI_BRANCH}"
 
 echo "Installing global dependencies"
+npm cache clean
 npm install -g web-component-tester@v3.1.7
+npm install -g bower
 #make sure wct-sauce plugin works with old version of wct
 npm install -g wct-sauce@1.6.0
+bower cache clean
 export NPM_ROOT=$(npm root -g)
 cp -r $NPM_ROOT/wct-sauce $NPM_ROOT/web-component-tester/node_modules
 
