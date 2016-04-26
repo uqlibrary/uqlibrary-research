5296b1f519a9802b334f60f636477ab1c5578a6b
Cameron Green <cam@uq.edu.au>
2016-04-26 11:14:05 +1000

    Auto live mode from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 /*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+ Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ Code distributed by Google as part of the polymer project is also
+ subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
 "use strict";
 
@@ -13,26 +13,43 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 var gulp = require("gulp");
 var browsersync = require("browser-sync");
 
+var browsersyncConfig = {
+  open: "external",
+  startPath: "/uqlibrary-research/demo",
+  host: "dev-app.library.uq.edu.au",
+  port: 9999,
+  server: {
+    baseDir: ["../"]
+  },
+  files: [
+    "elements/*.html",
+    "elements/*.js"
+  ]
+};
+
 // Watch files for changes & reload
-gulp.task("serve", [], function() {
-  console.log("serving...");
-  browsersync({
-    open: "external",
-    startPath: "/uqlibrary-research/demo",
-    host: "dev-app.library.uq.edu.au",
-    port: 9999,
-    server: {
-      baseDir: ["../"]
-    },
-    files: [
-      "elements/*.html",
-      "elements/*.js"
-    ]
-  });
+gulp.task("serve", [], function () {
+  console.log("Running server...");
+  browsersync(browsersyncConfig);
 });
 
+// Run the server, but comment out the mock data cookies
+// Note: For some reason it often requires a manual hard browser refresh
+// to switch from normal serving to this mode
+gulp.task("live", [], function () {
+  console.log("Running demonstration server...");
+  browsersyncConfig.rewriteRules = [
+    {
+      match: /(document\.cookie="UQLMockData)/g,
+      replace: "// $1"
+    }
+  ];
+  browsersync(browsersyncConfig);
+});
+
+
 // Build production files, the default task
-gulp.task("default", ["serve"], function(cb) {
+gulp.task("default", ["serve"], function (cb) {
 });
 
 // Load tasks for web-component-tester
@@ -42,6 +59,7 @@ require("web-component-tester").gulp.init(gulp);
 // Load custom tasks from the `tasks` directory
 try {
   require("require-dir")("tasks");
-} catch (err) {
+}
+catch (err) {
   // Do nothing
 }
